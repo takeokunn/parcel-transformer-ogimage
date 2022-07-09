a6e539840499706ae9bdd12245f36fd159bbc3f3
takeokunn <bararararatty@gmail.com>
2022-07-09 17:02:26 +0900

    Refactor codes

--- index.js
+++ index.js
@@ -1,48 +1,58 @@
 const url = require('url');
 const { Transformer } = require('@parcel/plugin');
 
-const getMetaTag = (html, property) => {
+const getMetaPropertyTag = (html, property) => {
     const regex = new RegExp(`<meta[^>]*property=["|']${property}["|'][^>]*>`, 'i');
     const results = regex.exec(html);
 
     if (!results) {
-	throw new Error(`Missing ${property}`);
+        throw new Error(`Missing ${property}`);
     }
 
     return results[0];
 };
+
+const getMetaNameTag = (html, name) => {
+    const regex = new RegExp(`<meta[^>]*name=["|']${name}["|'][^>]*>`, 'i');
+    const results = regex.exec(html);
+
+    if (!results) {
+        throw new Error(`Missing ${name}`);
+    }
+
+    return results[0];
+};
+
 
 const getMetaTagContent = (metaTagHtml) => {
     const contentRegex = /content="([^"]*)"/i;
     const results = contentRegex.exec(metaTagHtml);
 
     if (!results) {
-	throw new Error(`Missing content attribute in ${chalk.bold(metaTagHtml)}`);
+        throw new Error(`Missing content attribute in ${chalk.bold(metaTagHtml)}`);
     }
 
     return results[1];
 };
 
-
 module.exports = new Transformer({
     async transform({ asset }) {
-        const html = await asset.getCode();
-        const imageTags = ['og:image', 'twitter:image'];
+        const baseHtml = await asset.getCode();
         try {
-            const ogUrlTag = getMetaTag(html, 'og:url');
+            const ogUrlTag = getMetaPropertyTag(baseHtml, 'og:url');
+            const ogImageTag = getMetaPropertyTag(baseHtml, 'og:image');
+            const twitterImageTag = getMetaNameTag(baseHtml, 'twitter:image');
+
             const ogUrlContent = getMetaTagContent(ogUrlTag);
-            let patchedHtml = html;
-            imageTags.forEach(tag => {
-                try {
-                    const imageTag = getMetaTag(html, tag);
-                    const imageTagContent = getMetaTagContent(imageTag);
-                    const absoluteImageUrl = url.resolve(ogUrlContent, imageTagContent);
-                    const imageTagAbsoluteUrl = imageTag.replace(imageTagContent, absoluteImageUrl);
-                    patchedHtml = patchedHtml.replace(imageTag, imageTagAbsoluteUrl);
-                } catch (error) {
-                    console.log(error.message);
-                }
-            });
+            const ogImageContent = getMetaTagContent(ogImageTag);
+            const twtiterImageContent = getMetaTagContent(twitterImageTag);
+
+            const absoluteOgImageUrl = url.resolve(ogUrlContent, ogImageContent);
+            const ogImageTagAbsoluteUrl = ogImageTag.replace(ogImageContent, absoluteOgImageUrl);
+            const twitterImageTagAbsoluteUrl = twitterImageTag.replace(twtiterImageContent, absoluteOgImageUrl);
+
+            let patchedHtml = baseHtml.replace(ogImageTag, ogImageTagAbsoluteUrl);
+            patchedHtml = patchedHtml.replace(twitterImageTag, twitterImageTagAbsoluteUrl);
 
             asset.setCode(patchedHtml);
         } catch (error) {
